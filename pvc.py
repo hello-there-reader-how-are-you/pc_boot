a3ee8473fe81b2561efda24bc6b37eea612d2658
hello there <hello.there.reader.how.are.you@gmail.com>
2024-01-11 19:55:29 -0500

    spell

--- pvc.py
+++ pvc.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 from discord import Intents
 import RPi.GPIO as GPIO
 
-from my_secrests.py import *
+from my_secrets.py import *
 
 PC_RELAY_PIN = 17
 REBOOT_RELAY_PIN = 27
