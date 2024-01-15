8f8ca338715633a8bc28f02b9ed22b140c8da28e
hello there <hello.there.reader.how.are.you@gmail.com>
2024-01-15 00:07:23 -0500

    rework

--- pvc.py
+++ pvc.py
@@ -11,6 +11,7 @@ from my_secrets import *
 PC_RELAY_PIN = 14
 REBOOT_RELAY_PIN = 15
 
+current_state = 0
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PC_RELAY_PIN, GPIO.OUT)
@@ -20,6 +21,11 @@ GPIO.setup(REBOOT_RELAY_PIN, GPIO.OUT)
 GPIO.output(PC_RELAY_PIN, GPIO.LOW)
 GPIO.output(REBOOT_RELAY_PIN, GPIO.LOW)
 
+
+def toggle(pin):
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(5)
+    GPIO.output(pin, GPIO.LOW)
 
 # Create the Discord bot
 intents = Intents.all()
@@ -40,40 +46,35 @@ async def list(ctx):
 
 @bot.command(name='pc_on')
 async def on(ctx):
-    # Turn on the PC relay
-    GPIO.output(PC_RELAY_PIN, GPIO.HIGH)
-    await ctx.send('pc is now on')
+    if not current_state:
+        # Turn on the PC relay
+        toggle(PC_RELAY_PIN)
+        await ctx.send('pc is now on')
+    else:
+        await ctx.send('pc is already on')
 
 @bot.command(name='pc_off')
 async def off(ctx):
-    # Turn off the PC relay
-    GPIO.output(PC_RELAY_PIN, GPIO.LOW)
-    await ctx.send('pc is now off')
+    if current_state:
+        # Turn on the PC relay
+        toggle(PC_RELAY_PIN)
+        await ctx.send('pc is now off')
+    else:
+        await ctx.send('pc is already off')
 
 @bot.command(name='pc_set_state')
 async def pc_set_state(ctx, new_state: str):
     # Redefine the state of the PC relay based on user input
     if new_state.lower() == 'on':
-        GPIO.output(PC_RELAY_PIN, GPIO.HIGH)
+        current_state = 1
         await ctx.send('PC state redefined to ON!')
     elif new_state.lower() == 'off':
-        GPIO.output(PC_RELAY_PIN, GPIO.LOW)
+        current_state = 0
         await ctx.send('PC state redefined to OFF!')
     else:
         await ctx.send('Invalid state. Please use "on" or "off".')
 
-@bot.command(name='pc_toggle')
-async def toggle(ctx):
-    # Toggle the state of the PC relay
-    current_state = GPIO.input(PC_RELAY_PIN)
-    new_state = not current_state
-    GPIO.output(PC_RELAY_PIN, new_state)
 
-    # Send a message indicating the PC relay state change
-    if new_state:
-        await ctx.send('PC turned ON!')
-    else:
-        await ctx.send('PC turned OFF!')
 
 @bot.command(name='pc_restart')
 async def restart(ctx):
@@ -84,27 +85,30 @@ async def restart(ctx):
     GPIO.output(REBOOT_RELAY_PIN, GPIO.LOW)
     await ctx.send('Rebooting end')
 
+@bot.command(name='pc_reboot')
+async def restart(ctx):
+    await ctx.send('Rebooting begin')
+    # Activate the reboot relay for a short duration
+    GPIO.output(REBOOT_RELAY_PIN, GPIO.HIGH)
+    time.sleep(4)  # Adjust the sleep duration as needed
+    GPIO.output(REBOOT_RELAY_PIN, GPIO.LOW)
+    await ctx.send('Rebooting end')
 
 
 @bot.command(name='pc_state')
 async def pc_state(ctx):
     # Get the current state of the PC relay
-    current_state = GPIO.input(PC_RELAY_PIN)
     await ctx.send(f'PC is {"ON" if current_state else "OFF"}.')
 
 
 
 @bot.command(name='pc_50/50')
 async def fif_fif(ctx):
-    current_state = GPIO.input(PC_RELAY_PIN)
-
     # Check if the PC is already off, if yes, do nothing
     if not current_state:
         await ctx.send('PC is already OFF. Skipping 50/50.')
-        return
-
     # 50% chance of turning the PC off
-    if choice([True, False]):
+    elif choice([True, False]):
         await off(ctx)
         await ctx.send('Losing Power...')
     else:
